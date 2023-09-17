7c6cd25d7f20d430527a8bfc49784ee4f8a438f7
SHREYA-SHRIRAO <shreyashrirao1819@gmail.com>
2023-09-17 13:15:01 +0530

    added new blocks of code

--- first_program.py
+++ first_program.py
@@ -1,3 +1,12 @@
 import os
 from datetime import datetime as dt
 print(f"Hello python, the time now is {dt.now()}")
+
+
+name = "Vedika"
+
+def my_func_name(name):
+	print(name)
+
+if __name__ == '__main__':
+	my_func_name(name)
